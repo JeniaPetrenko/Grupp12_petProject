542a6c7709a89853098a2edfa2fdb49d8b7d7390
JeniaPetrenko <144125087+JeniaPetrenko@users.noreply.github.com>
2024-10-07 14:06:47 +0200

    1

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const ham = document.getElementById("hamburger");
 const nav = document.getElementById("navbar-links");
 const navTop = document.getElementById("navbar");
 
-ham.addEventListener("click", ()=>{
-    nav.classList.toggle("display");
-    navTop.classList.toggle("bgWhite");
-    document.body.classList.toggle("scrollLock");
-})
+ham.addEventListener("click", () => {
+  nav.classList.toggle("display");
+  navTop.classList.toggle("bgWhite");
+  document.body.classList.toggle("scrollLock");
+});
